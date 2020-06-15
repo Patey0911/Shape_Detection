1a986d0532fd2221ea6d7c606ad2564caa68ecf2
Patey0911 <mihalybogdanmatei@yahoo.com>
2020-06-15 19:30:20 +0300

    C

--- Popoe.py
+++ Popoe.py
@@ -12,16 +12,16 @@ while(True):
     l_h = cv2.getTrackbarPos("L-H", "Trackbars")
     u_s = cv2.getTrackbarPos("U-S", "Trackbars")
     imgGrey=cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    _,thrash=cv2.threshold(imgGrey, 130, 250,cv2.THRESH_BINARY)
+    _,thrash=cv2.threshold(imgGrey, 185, 180,cv2.THRESH_BINARY)
     contours,_=cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         approx=cv2.approxPolyDP(contour, 0.1* cv2.arcLength(contour, True), True)
         if (cv2.contourArea(contour)>1000 and cv2.contourArea(contour)<30000 and len(approx)>2):
-            cv2.drawContours(frame1, [approx], 0, (0,0,255), 3)
+            cv2.drawContours(frame1, [approx], 0, (0,0,255), 8)
             x=approx.ravel()[0]
             y=approx.ravel()[1]
             if(len(approx)==3):
-                cv2.putText(frame1, 'Triunghi', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (102, 255, 51), 2, cv2.LINE_AA)
+                cv2.putText(frame1, 'Triunghi', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (102, 255, 51), 5, cv2.LINE_AA)
             elif(len(approx)==4):
                 x ,y, w, h = cv2.boundingRect(approx)
                 aspectRatio = float(w)/float(h)
